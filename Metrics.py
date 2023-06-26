8c0ed41180f0bd8b16d90ae4dd77fc7f9351aa16
FalkAurel <137809006+FalkAurel@users.noreply.github.com>
2023-06-26 18:02:07 +0200

    Update Metrics.py
    Clean-Up

--- Metrics.py
+++ Metrics.py
@@ -5,11 +5,3 @@ class Metrics(object):
     @staticmethod
     def accuracy(yhat, y):
         return f"{np.mean(yhat.argmax(axis = 1) == y.argmax(axis = 1))}%"
-    
-if __name__ == "__main__":
-    a = np.array([[0.7, 0.2, 0.1],
-                  [0.5, 0.1, 0.4],
-                  [0.02, 0.9, 0.08]])
-    b = np.array([[1, 0, 0],
-                  [0, 1, 0],
-                  [0, 1, 0]])
