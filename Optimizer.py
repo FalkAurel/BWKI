909304e7ae5ea399fd6a846b3f08316897c479ea
FalkAurel <137809006+FalkAurel@users.noreply.github.com>
2023-06-29 13:16:47 +0200

    Update Optimizer.py
    LearningRateDecay und Momentum für SGD implementiert. 
    Hyperparameter sind nun "protected" und änderbar mit bestimmten  methoden.

--- Optimizer.py
+++ Optimizer.py
@@ -1,19 +1,60 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractclassmethod
+import numpy as np
 
 class Optimizer(ABC):
     
     @abstractmethod
-    def __init__(self, lernrate):
-        self.lernrate = lernrate
-    
+    def __init__(self, lernrate, momentum):
+        self._lernrate = lernrate
+        self._stepCount = 1
+        self._momentum = momentum
+        
     @abstractmethod
     def step(self, layer):
         pass
-
-class SGD(Optimizer):
-    def __init__(self, lernrate):
-        super().__init__(lernrate)
     
+    @abstractmethod
+    def learningRateDecay(self):
+        pass
+    
+    @abstractclassmethod
+    def setLearnRateDecay(cls, value):
+        pass
+    
+    @abstractmethod
+    def __repr__(self):
+        pass
+    
+class SGD(Optimizer):
+    
+    _learnRateDecay = 1e-7
+    
+    def __init__(self, lernrate, momentum):
+        super().__init__(lernrate, momentum)
+        
     def step(self, layer):
-        layer.weight -= layer.dweight * self.lernrate
-        layer.bias -= layer.dbias * self.lernrate
+        if self._momentum:
+            if not hasattr(layer, "_weightMomentum"):
+                layer._weightMomentum = np.zeros_like(layer._weight)
+                layer._biasMomentum = np.zeros_like(layer._bias)
+            updateWeight = self._momentum * layer._weightMomentum - self._lernrate * layer._dweight
+            updateBias = self._momentum * layer._biasMomentum - self._lernrate * layer._dbias
+            layer._weightMomentum = updateWeight
+            layer._biasMomentum = updateBias
+            layer._weight += updateWeight
+            layer._bias += updateBias
+        else:
+            layer._weight -= self._lernrate * layer._dweight
+            layer._bias -= self._lernrate * layer._dbias
+        return self
+    
+    def learningRateDecay(self):
+        self._lernrate = self._lernrate * 1. / (1. + self._learnRateDecay * self._stepCount)
+        self._stepCount += 1
+    
+    def __repr__(self):
+        return f"SGD(LearningRate={self._lernrate}, learningRateDecay={self._learnRateDecay}, momentum={self._momentum})"
+    
+    @classmethod
+    def setLearnRateDecay(cls, value):
+        cls._learnRateDecay = value
