b3a57090443f96b61c45b1d0d8c64c0e818e27e5
FalkAurel <137809006+FalkAurel@users.noreply.github.com>
2023-06-30 21:48:08 +0200

    Update Optimizer.py
    Clean-Up, Code Reduction

--- Optimizer.py
+++ Optimizer.py
@@ -1,60 +1,83 @@
-from abc import ABC, abstractmethod, abstractclassmethod
 import numpy as np
 
-class Optimizer(ABC):
+class Optimizer(object):
+    _learnRateDecay = 1e-3
     
-    @abstractmethod
-    def __init__(self, lernrate, momentum):
-        self._lernrate = lernrate
-        self._stepCount = 1
-        self._momentum = momentum
-        
-    @abstractmethod
-    def step(self, layer):
-        pass
-    
-    @abstractmethod
-    def learningRateDecay(self):
-        pass
-    
-    @abstractclassmethod
-    def setLearnRateDecay(cls, value):
-        pass
-    
-    @abstractmethod
-    def __repr__(self):
-        pass
-    
-class SGD(Optimizer):
-    
-    _learnRateDecay = 1e-7
-    
-    def __init__(self, lernrate, momentum):
-        super().__init__(lernrate, momentum)
+    def __init__(self, lernrate):
+        self.lernrate = lernrate
+        self._stepCount = 0
+        self._currentLearnRate = lernrate
         
     def step(self, layer):
-        if self._momentum:
-            if not hasattr(layer, "_weightMomentum"):
-                layer._weightMomentum = np.zeros_like(layer._weight)
-                layer._biasMomentum = np.zeros_like(layer._bias)
-            updateWeight = self._momentum * layer._weightMomentum - self._lernrate * layer._dweight
-            updateBias = self._momentum * layer._biasMomentum - self._lernrate * layer._dbias
-            layer._weightMomentum = updateWeight
-            layer._biasMomentum = updateBias
-            layer._weight += updateWeight
-            layer._bias += updateBias
-        else:
-            layer._weight -= self._lernrate * layer._dweight
-            layer._bias -= self._lernrate * layer._dbias
-        return self
+        raise NotImplementedError
     
     def learningRateDecay(self):
-        self._lernrate = self._lernrate * 1. / (1. + self._learnRateDecay * self._stepCount)
+        self._currentLearnRate = self.lernrate / (1. + self._stepCount * self._learnRateDecay)
         self._stepCount += 1
-    
-    def __repr__(self):
-        return f"SGD(LearningRate={self._lernrate}, learningRateDecay={self._learnRateDecay}, momentum={self._momentum})"
     
     @classmethod
     def setLearnRateDecay(cls, value):
         cls._learnRateDecay = value
+    
+    def __repr__(self):
+        raise NotImplementedError
+
+    
+class SGD(Optimizer):
+    """
+    Stochastic Gradient Descent(SGD). Vanilla-Optimizer. Dieser hier supported learningRateDecay; kontrollierte Ab-
+    nahme der Learning Rate und Momentum. Momentum ist echt interessant, da wir so aus einem lokalen Minimum(leicht-
+    er / eher) ausbrechen können. Momentum ist nur das vorherrige Update zu dem Parameter mit einem Faktor zwischen
+    0 - 1; wieviel davon übernommen werden soll.
+    """
+    _learnRateDecay = 1e-3
+    
+    def __init__(self, lernrate, momentum):
+        super().__init__(lernrate)
+        self._momentum = momentum
+        
+    def step(self, layer):
+        if self._momentum:
+            if not hasattr(layer, "weightMomentum"):
+                layer.weightMomentum = np.zeros_like(layer.weight)
+                layer.biasMomentum = np.zeros_like(layer.bias)
+            updateWeight = self._momentum * layer.weightMomentum - self._currentLearnRate * layer.dweight
+            updateBias = self._momentum * layer.biasMomentum - self._currentLearnRate * layer.dbias
+            layer.weightMomentum = updateWeight
+            layer.biasMomentum = updateBias
+            layer.weight += updateWeight
+            layer.bias += updateBias
+        else:
+            layer.weight -= self._currentLearnRate * layer.dweight
+            layer.bias -= self._currentLearnRate * layer.dbias
+    
+    def __repr__(self):
+        return f"SGD(LearningRate={self.lernrate}, learningRateDecay={self._learnRateDecay}, momentum={self._momentum})"
+
+
+class AdaGrad(Optimizer):
+    """
+    AdaGrad ist eine "Subform" des SGD. AdaGrad steht für adaptive Gradient. Wie der Name vermuten lässt, adaptiert
+    dieser. Dieses Adaption wird durch eine Normalisierung der Werte erreicht, indem man einen "Update-Verlauf" von
+    dem Lernprozess anfertigt. Wenn dieser in eine Richtung zu stark ansteigt, so wird die Optimierung in diese Rich-
+    tung immer langsamer.
+    Der Update-Verlauf entsteht durch den ZwischenSpeicher, der jedes mal mit dem Quadrat der aktuellen Gradienten
+    aktualisiert wird. Je größer dieser Wert wird, desto größer der Nenner.
+    """
+    _learnRateDecay = 1e-5
+    
+    def __init__(self, lernrate, epsilon = 1e-7):
+        super().__init__(lernrate)
+        self.epsilon = epsilon
+    
+    def step(self, layer):
+        if not hasattr(layer, "weightZwischenSpeicher"):
+            layer.weightZwischenSpeicher = np.zeros_like(layer.weight)
+            layer.biasZwischenSpeicher = np.zeros_like(layer.bias)
+        layer.weightZwischenSpeicher += layer.dweight**2
+        layer.biasZwischenSpeicher += layer.dbias**2
+        layer.weight -= self._currentLearnRate * layer.dweight / (np.sqrt(layer.weightZwischenSpeicher) + self.epsilon)
+        layer.bias -= self._currentLearnRate * layer.dbias / (np.sqrt(layer.biasZwischenSpeicher) + self.epsilon)
+    
+    def __repr__(self):
+        return f"AdaGrad(LearningRate={self.lernrate}, learningRateDecay={self._learnRateDecay}, epsilon={self.epsilon})"
