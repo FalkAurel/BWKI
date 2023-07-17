f4faa1da65a409a254fead6beea3902e9d9f6ae4
FalkAurel <137809006+FalkAurel@users.noreply.github.com>
2023-07-17 14:27:43 +0200

    Delete Metrics.py

--- Metrics.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Metrics(object):
-    
-    @staticmethod
-    def accuracy(yhat, y):
-        if y.ndim == 2:
-            y = y.argmax(axis = 1)
-        return np.mean(yhat.argmax(axis = 1) == y)
