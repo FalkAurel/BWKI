0ee902b9cd8c71411e2dea844e89bc8012b934a0
FalkAurel <137809006+FalkAurel@users.noreply.github.com>
2023-07-18 17:41:39 +0200

    Update convolution.py
    Convolution class wurde jetzt fertig gestellt mit Backward method. dweight und dbias werden als Attribute gespeichert. Optimizer updatet die weights und bias. Neben numpy wurde auch scipy hinzugefügt.

--- visualProcessing/convolution.py
+++ visualProcessing/convolution.py
@@ -1,13 +1,15 @@
 import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
+import scipy.signal
 
 #Quelle https://victorzhou.com/blog/intro-to-cnns-part-1/
 #Xavier Initialization: https://www.quora.com/What-is-an-intuitive-explanation-of-the-Xavier-Initialization-for-Deep-Neural-Networks
+#https://pavisj.medium.com/convolutions-and-backpropagations-46026a8f5d2c
 
 class Convolution(object):
-    def __init__(self, inputGröße, *, kernelGröße = 2, filterNum = 16):
+    def __init__(self, inputGröße, *, kernelGröße = 2):
         self.kernelGröße = kernelGröße
-        self.weight = np.random.randn(filterNum, kernelGröße, kernelGröße) / kernelGröße**2# Xavier Initialization
+        self.weight = np.random.randn(kernelGröße, kernelGröße) / kernelGröße**2# Xavier Initialization
         self.outputShape = (inputGröße[0] - kernelGröße + 1, inputGröße[1] - kernelGröße + 1)
         self.bias = np.random.randn(*self.outputShape)
     
@@ -18,13 +20,8 @@ class Convolution(object):
         return featureMap + self.bias
     
     def backward(self, gradient):
-        raise NotImplementedError
-    
-if __name__ == "__main__":
-    conv = Convolution((3, 3))
-    conv.weight = np.array([[1, 2,],
-                            [-1, 0]])
-    testMatrix = np.array([[1, 6, 2],
-                           [5, 3, 1],
-                           [7, 0, 4]])
-    out = conv.forward(testMatrix)
+        subArrays = sliding_window_view(self.input, window_shape=gradient.shape)
+        self.dweight = np.tensordot(subArrays, gradient, axes=([2, 3], [0, 1]))
+        self.dbias = gradient
+        dInput = scipy.signal.convolve2d(np.flip(self.weight), gradient, mode="full")
+        return dInput
