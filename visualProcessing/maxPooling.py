c36ff76fd0f0726526d198e3d5ab1ea13881b389
FalkAurel <137809006+FalkAurel@users.noreply.github.com>
2023-07-18 17:47:46 +0200

    Update maxPooling.py
    sauber machen

--- visualProcessing/maxPooling.py
+++ visualProcessing/maxPooling.py
@@ -17,19 +17,7 @@ class MaxPooling(object):
                 output[Y, X] = np.max(image[y:y+self.maxPoolingSize, x:x+self.maxPoolingSize])
                 self._maxOutputIndizes[Y, X] = np.argmax(image[y:y+self.maxPoolingSize, x:x+self.maxPoolingSize])
         return output
-    
-    """
-    Funktioniert nicht, Optimierung wurde noch nicht implementiert.
-    def forwardOptim(self, image):
-        self._input = image
-        h, w = image.shape
-        self._maxOutputIndeces1 = np.copy(np.zeros((h // self.maxPoolingSize, w // self.maxPoolingSize)))
-        y, x = image.shape
-        y //= self.maxPoolingSize
-        x //= self.maxPoolingSize
-        self._maxOutputIndizes = image[:y*self.maxPoolingSize, :x*self.maxPoolingSize].reshape(y, self.maxPoolingSize, x, self.maxPoolingSize).argmax(axis = 1).argmax(axis = 2)
-        return image[:y*self.maxPoolingSize, :x*self.maxPoolingSize].reshape(y, self.maxPoolingSize, x, self.maxPoolingSize).max(axis = (1, 3))
-    """
+        
     def backward(self, gradient):
         dInput = np.zeros_like(self._input)
         h, w = self._input.shape
@@ -39,23 +27,3 @@ class MaxPooling(object):
         maxIndex = np.unravel_index(self._maxOutputIndeces[Y, X].astype(int), (self.maxPoolingSize, self.maxPoolingSize))
         dInput[Y * self.maxPoolingSize + maxIndex[0], X * self.maxPoolingSize + maxIndex[1]] = gradient[Y, X]
         return dInput
-
-if __name__ == "__main__":
-    from time import perf_counter
-    m = MaxPooling(2)
-    test = np.array([[1, 2, 3 ,4],
-                     [5, 1, 2, 3],
-                     [4, 5, 1, 2],
-                     [3, 4, 5, 1]])
-    gradient = np.array([[2, -3],
-                         [4, 5]])
-    out = m.forward(test)
-    dInput = m.backward(gradient)
-    """
-    expected Output
-    [[ 0,  0,  0, -3],
-    [ 2,  0,  0,  0],
-    [ 0,  4,  0,  0],
-    [ 0,  0,  5,  0]]
-    """
-    
