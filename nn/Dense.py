caef5ccef837bf7b30f6e19f6a4ee27044012860
FalkAurel <137809006+FalkAurel@users.noreply.github.com>
2023-08-02 21:45:48 +0200

    Update Dense.py
    Implementing BatchNormalization. Tested on 4 different dnns, showed in all tests great results

--- nn/Dense.py
+++ nn/Dense.py
@@ -1,9 +1,6 @@
 import numpy as np
 
-class Layer(object):
-    pass
-
-class DenseLayer(Layer):
+class DenseLayer(object):
     def __init__(self, n_input, n_neuronen, l1WFaktor=0, l1BFaktor=0, l2WFaktor=0, l2BFaktor=0):
         self.weight = np.random.randn(n_input, n_neuronen) * 1e-3
         self.bias = np.zeros((1, n_neuronen))
@@ -57,6 +54,55 @@ class DropOutLayer(object):
 class FlattenLayer(object):
     def forward(self, inputs):
         self._inputShape = inputs.shape
+        inputs = inputs.reshape(self._inputShape[0], -1)
+        return inputs
+    
+    def backward(self, inputs):
+        inputs = np.squeeze(inputs)
+        return inputs.reshape(self._inputShape)
+    
+class BatchNormalizationLayer():
+    """
+    Improves training performance by reparametrising the underlying optimization problem. BatchNormalization
+    smoothens out the gradient, enabling gradient-based learning to take larger steps. -> Increases stability and makes
+    it less dependant on the choice of hyperparameters.
+    """
+    def __init__(self):
+        """
+        Please note that I'm substituting gamma and beta for weight and bias to make this module compa-
+        tible with the rest of the libary.
+        """
+        self.weight = 1
+        self.bias = 0
+        self.l1WFaktor = self.l1BFaktor = self.l2WFaktor = self.l2BFaktor = 0#this is implemented so that the model can be built.
+        
+    def forward(self, inputs):
+        """
+        Subtracting from the input its mean before dividing by the standard deviation of the input.
+        Finally multiplying it by the self.weight parameter and adding self.bias to it.
+        """
+        self.inputs = inputs
+        self.mean = np.mean(inputs, axis=0)
+        self.variance = np.var(inputs, axis=0)
+        self.stdDev = np.sqrt(self.variance + 1e-8)
+        self.normalizedInputs = (inputs - self.mean) / self.stdDev
+        return self.weight * self.normalizedInputs + self.bias
+    
+    def backward(self, gradient):
+        """
+        Backpropagation through the layer. We first compute the gradients of the loss with respect to
+        the normalized inputs, variance, and mean. Then we apply the chain rule to derive dweight,
+        dInput and dbias. As per usual, dbias is just the gradient as its derivative of the sum op-
+        eration is one.
+        """
+        N, D = gradient.shape
+        dNormalizedInputs = gradient * self.weight
+        dVariance = np.sum(dNormalizedInputs * (self.inputs - self.mean) * -0.5 * (self.variance + 1e-8)**(-1.5), axis=0)
+        dMean = np.sum(dNormalizedInputs * -1 / self.stdDev, axis=0) + dVariance * np.mean(-2 * (self.inputs - self.mean), axis=0)
+        dInput = dNormalizedInputs / self.stdDev + dVariance * 2 * (self.inputs - self.mean) / N + dMean / N
+        self.dweight = np.sum(gradient * self.normalizedInputs, axis=0)
+        self.dbias = np.sum(gradient, axis=0)
+        return dInput
         return inputs.reshape(-1)
     
     def backward(self, inputs):
