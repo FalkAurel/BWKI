358dfc8a2b2ad3b425904baf3759dc9a6f3ba6e4
FalkAurel <137809006+FalkAurel@users.noreply.github.com>
2023-08-10 21:06:46 +0200

    Update convolution.py
    Fixing the convLayer. The forwardPass is now able to work properly. Full support to the model has been added, it can be integrated into the model which is provided by the model module. 
    
    Issue to fix is the tradeoff between batch-support and nums of featureMaps. Currently we're implementing batch-support as this way we can integrate it into our model. The best thing i could manage to do in terms of implementing both batch-support and nums of featureMaps is a runtime of O(n^dimOfArray); meaning this is getting progressively worse as we stack more and more convLayers and therefore featureMaps. Once I figure out a viable solution to this problem, I'll be sure to implement it.

--- visualProcessing/convolution.py
+++ visualProcessing/convolution.py
@@ -1,12 +1,9 @@
 import numpy as np
 from numpy.lib.stride_tricks import sliding_window_view
 import scipy.signal
-from time import perf_counter
-
-#Quelle: https://pavisj.medium.com/convolutions-and-backpropagations-46026a8f5d2c
 
 class Convolution():
-    def __init__(self, inputGröße:tuple ,*,kernelGröße = 2, batchSize = 64):
+    def __init__(self, inputGröße:tuple ,*, kernelGröße = 2, batchSize = 64):
         """
         #Initilize Class
         """
@@ -19,21 +16,8 @@ class Convolution():
     
     def forward(self, image):
         """
-        Es wird eine Kopie vom Bild erzeugt, damit man es später in dem BackwardPass benutzen kann, um die dweights zu err
-        echnen und es als Vorlage für den dInput zu nutzen.
-        Forwardpass valide Kreuzkorrelation am Bild. Es wird eine featureMap erstellt. 
-        """
-        self.input = image
-        subArray = sliding_window_view(image, window_shape=(self.weight.shape))
-        featureMap = np.sum(self.weight * subArray, axis =(3, 4, 5))#entfernt 3 dimensionen -> 6dim wird zu 3dim
-        return featureMap + self.bias
-    
-    def forwardOptim(self, image):
-        """
-        Es wird eine Kopie vom Bild erzeugt, damit man es später in dem BackwardPass benutzen kann, um die dweights zu err
-        echnen und es als Vorlage für den dInput zu nutzen.
-        Forwardpass valide Kreuzkorrelation am Bild. Es wird eine featureMap erstellt.
-        Diese Implementation schafft kleinere windows, sodass diese nicht so Speicherintensiv sind. 
+        We create a copy of the image. The forwardPass is nothing more than the cross-correlation between the weights and
+        the image, resulting in a small part of the featureMap.
         """
         self.input = image
         featureMap = np.zeros(self.outputShape)
@@ -41,42 +25,18 @@ class Convolution():
             subArray = sliding_window_view(image[i], window_shape=self.weight.shape[1:])
             featureMap[i] = np.sum(self.weight[i] * subArray, axis=(2, 3))
         return featureMap + self.bias
-
     
     def backward(self, gradient):
         """
-        BackwardPass wird durchgeführt.
-        dInput ist nichts anderes als die 90° rotiertete volle Kreuzkorrelation zwischen der weight Matrix und dem
-        Gradienten
-        Die Ableitung einer Summe ist 1, weshalb der Gradient direkt für dbias übernommen wird
-        nach Kettenregel(1 * gradient).
+        dInput is the 90° rotated full cross-correlation between the weight and the gradient, resulting in a matrix of
+        shape image. dweight is the cross-correlation between the input and the gradient. dbias is just the gradient.
         """
         dInput = np.copy(self.input).astype(np.float64)
         self.dbias = gradient.sum(axis = 0, keepdims = True) / self.batchSize
         self.dweight = np.zeros(self.weight.shape)
         for batch in range(len(gradient)):
             subArrays = sliding_window_view(self.input[batch], window_shape=(gradient[batch].shape))
-            self.dweight[batch] = np.sum(subArrays * gradient[batch], axis = (2, 3))#davor wars gradient jetzt gradient[shape]
+            self.dweight[batch] = np.sum(subArrays * gradient[batch], axis = (2, 3))
             dInput[batch] = scipy.signal.convolve2d(gradient[batch], self.weight[batch], mode="full")
         self.dweight /= self.batchSize
         return dInput
-
-if __name__ == "__main__":
-    conv = Convolution((2, 8, 8))
-    test = np.random.randn(2, 8, 8)
-    test = np.array([[[1, 6, 2],
-                      [5, 3, 1],
-                      [7, 0, 4]],
-                     [[1, 6, 2],
-                      [5, 3, 1],
-                      [7, 0, 4]]])
-    
-    conv.weight = np.array([[[1, 2],
-                             [-1, 0]],
-                            [[1, 2],
-                             [-1, 0]]])
-    start = perf_counter()
-    out1 = conv.forwardOptim(test)
-    out = conv.forwardOptim(test)
-    gradient = np.random.randn(*out.shape)
-    dInput = conv.backward(gradient)
