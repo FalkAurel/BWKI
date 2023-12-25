e5f2af2a908f7ae6fc3671834bb211febd680bf2
Falk Aurel <137809006+FalkAurel@users.noreply.github.com>
2023-12-25 17:33:27 +0100

    Delete signalProcessing directory

--- signalProcessing/fourierFeatureMapping.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def fourierFeatureMapping(data, shape):
-    """
-    Takes a matrix as input and a shape tuple, which defines the size of the random gaussian matrix.
-    The last dimension of the filter has to match up with first dimension of the data.
-    """
-    randomGaussianMatrix = 2 * np.pi * np.random.randn(*shape) @ data
-    return np.vstack([np.cos(randomGaussianMatrix), np.sin(randomGaussianMatrix)])
